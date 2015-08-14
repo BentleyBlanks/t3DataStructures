4d68a634c448eb45af957f8da8115e8c9b0d7f04
官小宾 <bentleyjobs@gmail.com>
2015-08-14 23:27:04 +0800

    更新了表达式求值

---
+++ t3DataStructures/LinearList/t3ExpressionTest.h
@@ -0,0 +1,26 @@
+#include <LinearList/t3Expression.h>
+
+#include <Common/t3Test.h>
+#include <iostream>
+#include <string>
+
+//#define T3_TEST_POSTFIX
+#define T3_TEST_INFIX_TO_POSTFIX
+
+int main(int argc, char* argv[])
+{
+    t3Expression exp;
+    
+#ifdef T3_TEST_POSTFIX
+    exp.create("42/1-12*+22*- ");
+    
+    t3Log("Expression evaluate: %d", exp.evaluate());
+#endif
+    
+#ifdef T3_TEST_INFIX_TO_POSTFIX
+    exp.create("a/b-c+d*e-a*c ");
+    
+    exp.toPostfix();
+#endif
+    return 0;
+}
--- t3DataStructures/main.cpp
+++ t3DataStructures/main.cpp
@@ -1,4 +1,5 @@
 //#include <SparseMatrix/SparseMatrixTest.h>
 //#include <String/StringADTTest.h>
 //#include <LinearList/t3LinearListTest.h>
-#include <LinearList/t3MazeTest.h>
+//#include <LinearList/t3MazeTest.h>
+#include <LinearList/t3ExpressionTest.h>
