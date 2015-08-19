9e8160c6b271507d47aefaa7069e48d45f8d66a4
官小宾 <bentleyjobs@gmail.com>
2015-08-19 16:26:08 +0800

    更新了寻找等价类的程序

--- t3DataStructures/main.cpp
+++ t3DataStructures/main.cpp
@@ -3,4 +3,5 @@
 //#include <Test/t3LinearListTest.h>
 //#include <Test/t3MazeTest.h>
 //#include <Test/t3ExpressionTest.h>
-#include <Test/t3LinkListTest.h>
+//#include <Test/t3LinkListTest.h>
+#include <Test/t3EquivalenceTest.h>
